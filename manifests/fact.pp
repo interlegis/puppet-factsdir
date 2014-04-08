5115a40e5bec356e6b1616c14e10407fd9667efa
Fabio Rauber <fabiorauber@gmail.com>
2014-04-08 11:29:39 -0300

    Working module

--- manifests/fact.pp
+++ manifests/fact.pp
@@ -1,8 +1,12 @@
 #fact.pp
 
-define factsdir::fact ( $key,
+define factsdir::fact ( $key = $title,
                         $value ) {
 
-  
+ concat::fragment { "$title":
+   target => "/etc/facter/facts.d/factsdir.txt",
+   content => template('factsdir/factsdir.txt.erb'),
+   order => '10',
+ }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 
 class factsdir ( $facts = { } ) {
 
+  # Create required directories
   file { ['/etc/facter','/etc/facter/facts.d']:
     ensure => directory,
     owner => 'root',
@@ -9,7 +10,22 @@ class factsdir ( $facts = { } ) {
     mode => '0644',
   }
 
+  # Set up configuration file
+  concat { "/etc/facter/facts.d/$title.txt":
+    owner          => 'root',
+    group          => 'root',
+    mode           => '0444',
+  }
+
+  concat::fragment { "$title-header":
+    target => "/etc/facter/facts.d/$title.txt",
+    content => '# External facts file managed by Puppet
+',
+    order => '01',
+  }
+
+  # Create facts
   validate_hash($facts)
-  create_resources(factsdir::facts, $facts)
+  create_resources(factsdir::fact, $facts)
 
 }
